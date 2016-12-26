dfdd19efa38e5debe517c94cc4bf6eb4d19c1491
grc <276220309@qq.com>
2016-12-26 18:42:34 +0800

    4

--- app/src/main/java/com/example/admin/screen/MainActivity.java
+++ app/src/main/java/com/example/admin/screen/MainActivity.java
@@ -1,19 +1,15 @@
 package com.example.admin.screen;
 
 import android.content.Intent;
-import android.support.v7.widget.Toolbar;
 
 import com.example.admin.base.ui.BaseActivity;
 import com.example.admin.screen.joke.JokeActivity;
 import com.example.admin.screen.picture.FunPicActivity;
 
-import butterknife.BindView;
 import butterknife.OnClick;
 
 public class MainActivity extends BaseActivity {
 
-    @BindView(R.id.toolbar)
-    Toolbar toolbar;
 
     @Override
     public int getLayoutId() {
@@ -27,7 +23,6 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void setupView() {
-        setSupportActionBar(toolbar);
     }
 
     @Override
