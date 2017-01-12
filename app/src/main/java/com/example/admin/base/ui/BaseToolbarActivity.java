9792bf3a592a64797a44a9b52098537eb8d760a2
grc <276220309@qq.com>
2017-01-12 18:17:34 +0800

    test

---
+++ app/src/main/java/com/example/admin/base/ui/BaseToolbarActivity.java
@@ -0,0 +1,21 @@
+package com.example.admin.base.ui;
+
+import android.support.v7.widget.Toolbar;
+
+import com.example.admin.screen.R;
+
+import butterknife.BindView;
+
+/**
+ * Created by Admin on 2017/1/12.
+ */
+
+public abstract class BaseToolbarActivity extends BaseActivity{
+    @BindView(R.id.toolbar)
+    public Toolbar toolbar;
+
+    @Override
+    public void setupView() {
+        setSupportActionBar(toolbar);
+    }
+}
--- app/src/main/java/com/example/admin/screen/main/MainActivity.java
+++ app/src/main/java/com/example/admin/screen/main/MainActivity.java
@@ -8,12 +8,11 @@ import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 import com.example.admin.adapter.MainAdapter;
+import com.example.admin.base.ui.BaseToolbarActivity;
 import com.example.admin.entity.MainBean;
-import com.example.admin.base.ui.BaseActivity;
 import com.example.admin.screen.R;
 import com.example.admin.screen.joke.JokeActivity;
 import com.example.admin.screen.picture.FunPicActivity;
@@ -22,10 +21,8 @@ import java.util.ArrayList;
 
 import butterknife.BindView;
 
-public class MainActivity extends BaseActivity implements NavigationView.OnNavigationItemSelectedListener,MainContract.View{
+public class MainActivity extends BaseToolbarActivity implements NavigationView.OnNavigationItemSelectedListener,MainContract.View{
 
-    @BindView(R.id.toolbar)
-    Toolbar toolbar;
     @BindView(R.id.nav_view)
     NavigationView navView;
     @BindView(R.id.drawer_layout)
@@ -50,7 +47,6 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
 
     @Override
     public void setupView() {
-        setSupportActionBar(toolbar);
         if (navView != null) {
             navView.setNavigationItemSelectedListener(this);
             ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
